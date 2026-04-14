cd2002961513bea6bf87b42520f634efc9376e52
Sigge Brandt <siggebrandt@icloud.com>
2026-04-14 11:47:08 +0200

    merges

--- public/script.js
+++ public/script.js
@@ -2,9 +2,9 @@ const messageView = document.getElementById("messageView");
 const allMessagesView = document.getElementById("allMessagesView");
 const messagePerson = document.querySelectorAll(".messagePerson");
 
-messagePerson.forEach(person => {
-    person.addEventListener("click", function () {
-        messageView.style.display = "flex";
-        allMessagesView.style.display = "none";
-    })
-})
+messagePerson.forEach((person) => {
+  person.addEventListener("click", function () {
+    messageView.style.display = "block";
+    allMessagesView.style.display = "none";
+  });
+});
