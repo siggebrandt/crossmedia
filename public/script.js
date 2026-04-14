37b1e641ebf2f2debb874c75b35bb3144bd69d29
Sigge Brandt <siggebrandt@icloud.com>
2026-04-14 11:51:32 +0200

    .

--- public/script.js
+++ public/script.js
@@ -2,9 +2,16 @@ const messageView = document.getElementById("messageView");
 const allMessagesView = document.getElementById("allMessagesView");
 const messagePerson = document.querySelectorAll(".messagePerson");
 
+const headerBack = document.querySelector("#headerBack");
+
 messagePerson.forEach((person) => {
   person.addEventListener("click", function () {
     messageView.style.display = "block";
     allMessagesView.style.display = "none";
   });
+});
+
+headerBack.addEventListener("click", function () {
+  messageView.style.display = "none";
+  allMessagesView.style.display = "block";
 });
