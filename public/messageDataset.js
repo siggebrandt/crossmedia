7831d12caa47d44e24e6e3505fd5ff8b34d636b0
Glassgobben <markuscehic21@gmail.com>
2026-04-23 15:05:53 +0200

    ändra dataset
    ändra datasetet med meddelanden från json till js

---
+++ public/messageDataset.js
@@ -0,0 +1,24 @@
+[
+    {
+        "name": "Anna",
+        "messages": [
+            {
+                "id": 1,
+                "text": "Jag har fått tips att Johannes Karlsson har något fuffens på gång. Du läser väl en av hans kurser, kan inte du se om du får reda på något mer?"
+            },
+            {
+                "id": 2,
+                "text": "hej"
+            }
+        ]
+    },
+    {
+        "name": "player",
+        "messages": [
+            {
+                "id": 1,
+                "text": "Ja"
+            }
+        ]
+    }
+]
