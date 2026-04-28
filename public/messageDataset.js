cb832625ece468b64295cdb29ca18a2055c8c68a
Glassgobben <markuscehic21@gmail.com>
2026-04-28 10:02:38 +0200

    ändrade o la till meddelanden

--- public/messageDataset.js
+++ public/messageDataset.js
@@ -6,40 +6,44 @@
                 id: 1,
                 text: "Jag har fått tips att Johannes Karlsson har något fuffens på gång. Du läser väl en av hans kurser, kan inte du se om du får reda på något mer?"
             },
-            {
-                id: 2,
-                text: "Dags att sätta igång att undersöka Johannes, är du på? Jag har gjort lite efterforskningar och om du följer mina instruktioner så kommer det här gå bra! Är du redo?"
-            },
             // spelaren svarar
             {
-                id: 3, //GLÖM INTE LÄGGA IN VART KONTORET ÄR HÄR!!!!
-                text: "Bra! Johannes kontor står tomt nu i 20 min medan han är på lunch. Kontoret ligger på /* INSERT KONTORSLOKAL HÄR */, ta dig dit och se om du kan hitta någon ledtråd! Du kan väl höra av dig om du hittar något. Som vanligt behöver du ta bild på allting du hittar och kom ihåg att lämna allt precis så som du hittade det så han inte kommer på dig, tänk på din journalistiska integritet. P.S. det kan vara bra att ta bild på många potentiella ledtrådar."
+                id: 2,
+                text: "Driver du allt jag gjort för dig, men vill du inte så hittar jag någon annan. Men då kan du skita i att dyka upp på måndag…"
+            },
+            {
+                id: 3,
+                text: "Du är bäääst!"
+            }
+            {
+                id: 4, //GLÖM INTE LÄGGA IN VART KONTORET ÄR HÄR!!!!
+                text: "Johannes kontor står tomt nu i 20 min medan han är på lunch. Kontoret ligger på /* INSERT KONTORSLOKAL HÄR */, ta dig dit och se om du kan hitta någon ledtråd! Du kan väl höra av dig om du hittar något. Som vanligt behöver du ta bild på allting du hittar och kom ihåg att lämna allt precis så som du hittade det så han inte kommer på dig, tänk på din journalistiska integritet. P.S. det kan vara bra att ta bild på många potentiella ledtrådar."
             },
             // kodinmatning
             {
-                id: 4,
+                id: 5,
                 text: "Vafan är det där du har hittat? Det där kan vi ju inte använda till något!!"
             },
             {
-                id: 5,
+                id: 6,
                 text: "Eller vänta"
             },
             {
-                id: 6,
+                id: 7,
                 text: "Är det inte..."
             },
             {
-                id: 7,
+                id: 8,
                 text: "Orkanenbiblioteket??"
             }
             // kodinmatning
             {
-                id: 8,
+                id: 9,
                 text: "Vad skumt, bara en flashbacktråd. Hittade du inte något mer på kontoret?"
             },
             // spelaren hittar brevet
             {
-                id: 9,
+                id: 10,
                 text: "Det kommer upp 3 blanka rutor - Som tur är det ett kort lösenord, skriv in det du hittat!"
             }
             // hemligheten avslöjas
@@ -48,14 +52,16 @@
     {
         name: "player",
         messages: [
+            // Annna
             {
                 id: 1,
-                text: "Ja"
+                text: "Ja det gör jag, ooo spännande jag löser"
             },
             {
                 id: 2,
-                text: "Nej"
+                text: "Ja det gör jag men nä vill inte gräva i hans business!"
             },
+            // Bodil
             {
                 id: 3,
                 text: "Behöver hjälp i Orkanenbiblioteket"
